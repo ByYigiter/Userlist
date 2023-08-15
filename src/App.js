6f7157b78be0a12a01989f6f4050caf8eb88dc0f
Yigit Er <mr.yigiter222@gmail.com>
2023-08-15 22:09:43 +0300

    new

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import HomePage from './pages/HomePage';
+import HomePage2 from './pages/HomePage2';
 
 function App() {
   return (
     <div className="App">
-     <HomePage/>
+     {/* <HomePage/> */}
+     <HomePage2/>
     </div>
   );
 }
---
+++ src/pages/HomePage2.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import Counter from '../component/Counter'
+
+
+export default class HomePage2 extends Component {
+  render() {
+    return (
+      <div>
+        <Counter/>
+      </div>
+    )
+  }
+}
