e2c2675668de86a50f002a1c5c21d440be1ec49d
Yigit Er <mr.yigiter222@gmail.com>
2023-08-18 14:47:02 +0300

    useEffect

--- src/component/Counter.js
+++ src/component/Counter.js
@@ -1,9 +1,19 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
 
 function Counter() {
-  return (
-    <div>Counter</div>
-  )
+	const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    console.log("useEffect");
+    //componenet update olduğunda
+    document.title=`${count} times click`
+  })
+	return (
+		<div>
+			<p>butona {count} click edildi</p>
+			<button onClick={() => setCount(count + 1)}>Click</button>
+		</div>
+	);
 }
 
-export default Counter
+export default Counter;
