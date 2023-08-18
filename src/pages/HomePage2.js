0ee156975a33b382e5428f2cab4cbd0ebaa97ea0
Yigit Er <mr.yigiter222@gmail.com>
2023-08-18 15:35:01 +0300

    todo app

--- src/pages/HomePage2.js
+++ src/pages/HomePage2.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react'
 import Counter from '../component/Counter'
+import Todo from '../component/Todo'
 
 
 export default class HomePage2 extends Component {
   render() {
     return (
       <div>
-        <Counter/>
+        {/* <Counter/> */}
+        <Todo/>
       </div>
     )
   }
